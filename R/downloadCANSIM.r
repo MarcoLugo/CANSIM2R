1c5935fc78e86207961ea8faf9922fba9e9f2e6d
MarcoLugo <malr87@hotmail.com>
2017-10-21 11:10:28 -0400

    Fixed bug: CANSIM tables with leading zeroes
    paste0 uses as.character for integers and truncates the leading zeroes. This was causing the table names to be incorrect and therefore not downloaded from CANSIM. Special thanks to Soheil Mahmoodzadeh for providing a proof-of-concept for the bug.

--- R/downloadCANSIM.r
+++ R/downloadCANSIM.r
@@ -1,17 +1,18 @@
 downloadCANSIM <- function(cansimTableNumber){
   temp <- tempfile()
   url <- "http://www20.statcan.gc.ca/tables-tableaux/cansim/csv/"
-  filename <- paste0("0", cansimTableNumber, "-eng")
+  cansimTableNumberString <- sprintf("%07d", as.numeric(cansimTableNumber)) #Put the correct amount of leading zeroes; paste0 uses as.character which truncates leading zeroes from integers (special thanks to Soheil soheil Mahmoodzadeh for reporting the bug)
+  filename <- paste0("0", cansimTableNumberString, "-eng")
   url <- paste0(url, filename, ".zip")
   download.file(url, temp, quiet = TRUE)
   data <- read.csv(unz(temp, paste0(filename, ".csv") ), stringsAsFactors = FALSE)
   unlink(temp)
-
+  
   data <- createStatCanVariables(data)
-
+  
   data$Vector <- NULL
   data$Coordinate <- NULL
   suppressWarnings(data$Value <- as.numeric(data$Value))
-
+  
   return(data)
 }
