3436764b233412ad9bd42f370d51346f7cfb61e0
Marco Lugo <marco.lugo.rodriguez@umontreal.ca>
2018-07-13 21:47:52 -0400

    Adapt downloadCANSIM to latest change by StatCan (session ID no longer needed)

--- R/getCANSIM.r
+++ R/getCANSIM.r
@@ -13,8 +13,7 @@
 #' @param raw - download the CANSIM table as-is, skipping all processing, FALSE by default.
 #' @return data frame containing CANSIM table.
 #' @examples
-#' getCANSIM("23-10-0238", showLabels = FALSE)
-#' getCANSIM("23-10-0238", raw = TRUE)
+#' getCANSIM("23-10-0238")
 #' getCANSIM("23100238")
 #' @export
 getCANSIM <- function(cansimTableNumber='', showLabels = TRUE, raw = FALSE){
